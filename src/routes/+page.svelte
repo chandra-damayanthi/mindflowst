32582236d2070d97ffc2f15b10907baa1f37b8ff
Chandra Damayanthi <cdamayanthi819@gmail.com>
2023-10-29 11:02:39 +0530

    Create starting version of the app on the home +page.svelte file

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,34 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<style>
+    h1 {
+        font-family: Arial, Helvetica, sans-serif;
+        letter-spacing: -1px;
+        color: rgb(134, 109, 191);
+    }
+</style>
+
+<script lang="ts">
+    export let items:string[] = [];
+    let workThought = '';
+     function addWorkThought() {
+        if (workThought.trim() !== '') {
+            items = [...items, workThought];
+            workThought = '';
+        }
+    }
+
+</script>
+
+<h1>Mindflowst</h1>
+<ul>
+    {#each items as item (item)}
+        <li>{item}</li>
+    {/each}
+</ul>
+<label>
+    Workthought 
+    <input type="text" bind:value={workThought}>
+</label>
+
+
+
+<button on:click={addWorkThought}>Add</button>
