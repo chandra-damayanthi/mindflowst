dc6c135ee03071245dc09d3fc46ccc9fc23a2983
Chandra Damayanthi <cdamayanthi819@gmail.com>
2023-10-29 16:12:39 +0530

    Update 'vite.config.ts' file to configure the app to run inside the docker container

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,16 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+ base: "/",
+ plugins: [sveltekit()],
+ preview: {
+  port: 5173,
+  strictPort: true,
+ },
+ server: {
+  port: 5173,
+  strictPort: true,
+  host: true,
+  origin: "http://0.0.0.0:5173",
+ },
 });
